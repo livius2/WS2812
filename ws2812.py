0398ecfb42427c6e32b9f8e172591beb90f59bd6
Karol Bieniaszewski <liviuslivius@poczta.onet.pl>
2021-02-22 16:07:12 +0100

    Update ws2812.py
    follow name convention

--- ws2812.py
+++ ws2812.py
@@ -18,18 +18,19 @@ class WS2812:
         sleep_us(60)  # wait for the reset time
         self.rmt.pulses_send(tuple(duration), (1, 0) * (len(data) * 24))
 
+
 class WS2812_immutable:
-    def __init__(self, led_count, pin, channel=3):
+    def __init__(self, pixel_count, pin, channel=3):
         from machine import RMT
         self.rmt = RMT(channel=channel, gpio=pin, tx_idle_level=0)
-		self.led_count = led_count
-		self.duration = bytearray(led_count * 24 * 2)
-		self.pulse_data = (1, 0) * (led_count * 24)
+		self.pixel_count = pixel_count
+		self.duration = bytearray(pixel_count * 24 * 2)
+		self.pulse_data = (1, 0) * (pixel_count * 24)
 
     def show(self, data):
         from time import sleep_us
-        if len(data)!=self.led_count:
-            raise ValueError("Param 'data' have wrong size. Expected: " + str(self.led_count) + " but is: " + str(len(data)))
+        if len(data)!=self.pixel_count:
+            raise ValueError("Param 'data' have wrong size. Expected: " + str(self.pixel_count) + " but is: " + str(len(data)))
         index = 0
         for pixel in data:
             for byte in pixel:
