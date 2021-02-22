56496430aff26c9fc17f288647ba7b7f436a01cd
Karol Bieniaszewski <liviuslivius@poczta.onet.pl>
2021-02-22 15:30:00 +0100

    Update ws2812.py
    Immutable version with fixed led count, minimize memory consumption

--- ws2812.py
+++ ws2812.py
@@ -1,9 +1,3 @@
-#
-# class for driving WS2812 Neopixel device on a Pycom
-# module using the RMT interface
-#
-
-
 class WS2812:
     def __init__(self, pin, channel=3):
         from machine import RMT
@@ -23,3 +17,27 @@ class WS2812:
                     mask >>= 1
         sleep_us(60)  # wait for the reset time
         self.rmt.pulses_send(tuple(duration), (1, 0) * (len(data) * 24))
+
+class WS2812_immutable:
+    def __init__(self, led_count, pin, channel=3):
+        from machine import RMT
+        self.rmt = RMT(channel=channel, gpio=pin, tx_idle_level=0)
+		self.led_count = led_count
+		self.duration = bytearray(led_count * 24 * 2)
+		self.pulse_data = (1, 0) * (led_count * 24)
+
+    def show(self, data):
+        from time import sleep_us
+        if len(data)!=self.led_count:
+            raise ValueError("Param 'data' have wrong size. Expected: " + str(self.led_count) + " but is: " + str(len(data)))
+        index = 0
+        for pixel in data:
+            for byte in pixel:
+                mask = 0x80
+                while mask:
+                    self.duration[index:index+2] = \
+                        b'\x08\x04' if byte & mask else b'\x04\x08'
+                    index += 2
+                    mask >>= 1
+        sleep_us(60)  # wait for the reset time
+        self.rmt.pulses_send(tuple(self.duration), self.pulse_data)
