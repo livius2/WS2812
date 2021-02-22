2fa21ac63b51a44a9bec5a4eca8f6a0063f33b39
Karol Bieniaszewski <liviuslivius@poczta.onet.pl>
2021-02-22 18:22:08 +0100

    Update ws2812.py
    merge all into one class

--- ws2812.py
+++ ws2812.py
@@ -1,31 +1,12 @@
 class WS2812:
-    def __init__(self, pin, channel=3):
-        from machine import RMT
-        self.rmt = RMT(channel=channel, gpio=pin, tx_idle_level=0)
-
-    def show(self, data):
-        from time import sleep_us
-        duration = bytearray(len(data) * 24 * 2)
-        index = 0
-        for pixel in data:
-            for byte in pixel:
-                mask = 0x80
-                while mask:
-                    duration[index:index+2] = \
-                        b'\x08\x04' if byte & mask else b'\x04\x08'
-                    index += 2
-                    mask >>= 1
-        sleep_us(60)  # wait for the reset time
-        self.rmt.pulses_send(tuple(duration), (1, 0) * (len(data) * 24))
-
-
-class WS2812_immutable:
     def __init__(self, pixel_count, pin, channel=3):
         from machine import RMT
         self.rmt = RMT(channel=channel, gpio=pin, tx_idle_level=0)
+        self.resize(pixel_count)
+
+    def resize(self, pixel_count):
         self.pixel_count = pixel_count
         self.duration = bytearray(pixel_count * 24 * 2)
-        self.pulse_data = (1, 0) * (pixel_count * 24)
 
     def show(self, data):
         from time import sleep_us
@@ -41,4 +22,4 @@ class WS2812_immutable:
                     index += 2
                     mask >>= 1
         sleep_us(60)  # wait for the reset time
-        self.rmt.pulses_send(tuple(self.duration), self.pulse_data)
+        self.rmt.pulses_send(tuple(self.duration), start_level=1)
